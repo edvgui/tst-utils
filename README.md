3630cb15118499f6507a5d1aaf0c7d4b36a348c0
Dariush <45129008+Riushda@users.noreply.github.com>
2024-03-03 18:10:18 +0100

    Documentation and main script improved (#1)
    * documentation and main script improved
    
    * input and output folder are now arguments
    
    * Apply suggestions from code review
    
    Reviewer suggestions
    
    Co-authored-by: Guillaume <edvgui@gmail.com>
    
    * tst-utils ascii drawing now fits in default terminal width
    
    * adapt readme help output
    
    * input folder is now mandatory argument
    
    ---------
    
    Co-authored-by: Guillaume <edvgui@gmail.com>

--- README.md
+++ README.md
@@ -1,22 +1,87 @@
 # TST Utils
 
+## Introduction
+
 This project contains some helpers to parse a trade republic tst file (pdf report) and use the output data to fill in the belgian TST file that should be sent to state.
 
 This code-base is purely opportunistic, and will rely on the simplest tools it can find to reach its goal.  Consistency of the technology used is not the primary goal, having a solution that works is.
 
-## Parsing Trade Republic transaction report
+## Prerequisites
 
-This is done in python, using pypdf and some naive regexes.  There is no guarantee it will continue to work in the future.
+The requirements are: 
+```
+- python>=3.10
+- podman 
+```
+Some tweaking with the gmail api is also necessary, see [tst-sender-readme](./tst-sender/README.md).
+
+## Input files 
+
+The script requires various input files in order to generate a correct TST pdf.
+The complete list is the following:
+
+- Personal information file, as json payload, with the following format:
+```
+{
+    "fullName": "FIRSTNAME LASTNAME",
+    "nationalRegisterNumber": "",
+    "address": [""]
+}
+```
+
+- Signature file as png or jpg, in order to sign the TST file.
+
+- The credentials json file used to access your gmail account, more information available [here](./tst-sender/README.md).
+
+## Quick start
+
+To run the bash script, just run:
+
+```
+sh sync.sh
+```
+
+The options are defined as follow:
+
+```
+[xxxxxx@xxxxxx tst-utils]$ sh sync.sh -h
+Tool to fill in belgian TST file from trade republic monthly report.
+This tool also creates a draft mail with the generated document and a qr code to pay the tax.
+
+Syntax: sh sync.sh [-h|s|p|c] input_folder [output_folder]
+options:
+h     Display this help and exit.
+s     Set the signature image file path, default value is 'data/signature.jpg'.
+p     Set the personal informations json file path, default value is 'data/citizen.json'.
+c     Set the Gmail credentials json file path, default value is 'data/credentials.json'.
+arguments:
+input_folder     Set the input folder for the pdf(s), this argument is mandatory.
+output_folder    Set the output folder for the generated pdf(s) and qr code, default value is 'output'.
+
+Please make sure to provide options before arguments.
+```
+
+
+## Components
+### Parsing Trade Republic transaction report
+
+This is done in python, using pypdf and some naive regexes. There is no guarantee it will continue to work in the future.
 
 See [tr-report-parser](./tr-report-parser/).
 
-## Filling in TST form
+### Filling in TST form
 
 This one is written in Javascript, and runs with node, inside a podman container.  The tool relies on the pdf-lib library, it is highly dependant on the form file used, and has only been tested with https://finance.belgium.be/sites/default/files/Changement%20de%20compte%20formulaire%20TST%20EN.pdf
 
 See [tst-filler](./tst-filler/).
 
-## Send TST form
+### Generate QR code for payment
+
+This is done in python, using qrcode library.
+
+See [tst-qr](./tst-qr/).
+
+### Send TST form
 
 This one is written in python, using gmail api to prepare a draft email, that can be sent to the belgian administration.  The tst form filled is attached to the email.
 
--- sync.sh
+++ sync.sh
@@ -2,9 +2,86 @@
 
 set -e
 
-# Read input arguments
+# Define Help function
+
+Help()
+{
+   # Display Help
+   echo "Tool to fill in belgian TST file from trade republic monthly report."
+   echo "This tool also creates a draft mail with the generated document and a qr code to pay the tax."
+   echo
+   echo "Syntax: sh sync.sh [-h|s|p|c] input_folder [output_folder]"
+   echo "options:"
+   echo "h     Display this help and exit."
+   echo "s     Set the signature image file path, default value is 'data/signature.jpg'."
+   echo "p     Set the personal informations json file path, default value is 'data/citizen.json'."
+   echo "c     Set the Gmail credentials json file path, default value is 'data/credentials.json'."
+   echo "arguments:"
+   echo "input_folder     Set the input folder for the pdf(s), this argument is mandatory."
+   echo "output_folder    Set the output folder for the generated pdf(s) and qr code, default value is 'output'."
+   echo
+   echo "Please make sure to provide options before arguments."
+   echo
+}
+
+# Set default values for options
+SIGNATURE_FILE="$(pwd)/data/signature.jpg"
+PERSONAL_INFOS_FILE="$(pwd)/data/citizen.json"
+CREDENTIALS_FILE="$(pwd)/data/credentials.json"
+
+# Traverse arguments
+while getopts ":hs:p:c:" option; do
+   case $option in
+      h) # display Help
+        Help
+        exit;;
+      s) # Signature file
+        SIGNATURE_FILE="$(pwd)/$OPTARG";;
+      p) # Personal informations file
+        PERSONAL_INFOS_FILE="$(pwd)/$OPTARG";;
+      c) # Gmail credentials file
+        CREDENTIALS_FILE="$(pwd)/$OPTARG";;
+      \?) # Invalid option
+        echo "Error: Invalid option '-$OPTARG'"
+        echo "Run 'sh sync.sh -h' to get more information about existing options."
+        exit;;
+   esac
+done
+
+echo " ________  ______   ________      __    __  ________  ______  __        ______  ";
+echo "/        |/      \ /        |    /  |  /  |/        |/      |/  |      /      \ ";
+echo "\$\$\$\$\$\$\$\$//\$\$\$\$\$\$  |\$\$\$\$\$\$\$\$/     \$\$ |  \$\$ |\$\$\$\$\$\$\$\$/ \$\$\$\$\$\$/ \$\$ |     /\$\$\$\$\$\$  |";
+echo "   \$\$ |  \$\$ \__\$\$/    \$\$ | _____ \$\$ |  \$\$ |   \$\$ |     \$\$ |  \$\$ |     \$\$ \__\$\$/ ";
+echo "   \$\$ |  \$\$      \    \$\$ |/     |\$\$ |  \$\$ |   \$\$ |     \$\$ |  \$\$ |     \$\$      \ ";
+echo "   \$\$ |   \$\$\$\$\$\$  |   \$\$ |\$\$\$\$$/ \$\$ |  \$\$ |   \$\$ |     \$\$ |  \$\$ |      \$\$\$\$\$\$  |";
+echo "   \$\$ |  /  \__\$\$ |   \$\$ |       \$\$ \__\$\$ |   \$\$ |    _\$\$ |_ \$\$ |____ /  \__\$\$ |";
+echo "   \$\$ |  \$\$    \$\$/    \$\$ |       \$\$    \$\$/    \$\$ |   / \$\$   |\$\$      |\$\$    \$\$/ ";
+echo "   \$\$/    \$\$\$\$\$\$/     \$\$/         \$\$\$\$\$\$/     \$\$/    \$\$\$\$\$\$/ \$\$\$\$\$\$$/  \$\$\$\$\$\$/  ";
+echo "                                                                                ";
+echo "                                                                                ";
+
+# shift (remove) all the processed parameters (variable $OPTIND)
+shift $(($OPTIND - 1))
+
+# Read input and output folder arguments
+if [ -z "$1" ]; then
+  # If input folder is not set, fail.
+  echo "Please provide at least an input folder as argument."
+  exit 1
+fi
+
 INPUT_DIR=$( cd -- "$1" > /dev/null && pwd )
-OUTPUT_DIR=$( cd -- "$2" > /dev/null && pwd )
+
+if [ -z "$2" ]; 
+then 
+  # If output folder is not set, default to 'output'
+  OUTPUT_DIR="$(pwd)/output"; 
+else
+  OUTPUT_DIR="$(pwd)/$2"
+fi
+
+# Create output dir only if it doesn't exist
+mkdir -p $OUTPUT_DIR
 
 # Resolve the dir where this file belongs
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -41,8 +118,8 @@ for src_file in "$INPUT_DIR"/*.pdf; do
         echo "$data" | \
             ./tst-filler/run \
             --form-file data/form-original.pdf \
-            --signature-file data/signature.jpg \
-            --tax-person data/citizen.json \
+            --signature-file $SIGNATURE_FILE \
+            --tax-person $PERSONAL_INFOS_FILE \
             --tax-data - \
             data/$(basename -- "$src_file")
         mv data/$(basename -- "$src_file") "$dst_file"
@@ -51,9 +128,9 @@ for src_file in "$INPUT_DIR"/*.pdf; do
         echo "$data" | \
             ./tst-sender/venv/bin/python \
             tst-sender/send_tst.py \
-            --app-credentials data/credentials.json \
+            --app-credentials $CREDENTIALS_FILE \
             --tax-data - \
-            --tax-person data/citizen.json \
+            --tax-person $PERSONAL_INFOS_FILE \
             "$dst_file"
         
         notify-send tst-sender "Created draft email for $dst_file"
@@ -65,7 +142,7 @@ for src_file in "$INPUT_DIR"/*.pdf; do
             ./tst-qr/venv/bin/python \
             tst-qr/create_qrcode.py \
             --tax-data - \
-            --tax-person data/citizen.json \
+            --tax-person $PERSONAL_INFOS_FILE \
             "$qr_code"
         
         notify-send tst-qr "Created payment qr code: $qr_code"
